5264894ac2fa9843613e420290eb1f1d77cb3f16
ETAvadh <developer.empiere@gmail.com>
2025-03-21 18:51:13 +0530

    - main: Integrated getUser api with the UI and displayed the data. - REST User: Implemented getUser api creating service function. - Model User: Added user model to map the api response. - pubspec: Added and registered necessary package.

---
+++ lib/rest_user.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'package:user_http_test/model_user.dart';
+
+class RESTUser {
+  static Future<ModelUser?> getUser() async {
+    const String tag = "get_user";
+    ModelUser? data;
+
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/users/1');
+    log('$tag URL : GET: $url');
+
+    try {
+      final response = await http.get(url);
+      log('$tag response : ${response.body}');
+
+      final decodedResult = json.decode(response.body);
+      data = ModelUser.fromJson(decodedResult);
+      return data;
+    } catch (e) {
+      log('$tag error: ${e.toString()}');
+      return null;
+    }
+  }
+}
